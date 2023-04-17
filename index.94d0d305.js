7dfd42f02bdd23bbc51f246e2767a2af87e63714
Dmytro-Hlushchenko <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-04-17 09:27:30 +0000

    Deploying to gh-pages from @ Dmytro-Hlushchenko/goit-js-hw-11@c10da00debc4e9431964483120c4b39511939800 🚀

---
+++ index.94d0d305.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("search-form");document.querySelector("button");e.addEventListener("submit",(function(e){e.preventDefault(),console.log(e.currentTarget.elements.searchQuery.value)}))}();
+//# sourceMappingURL=index.94d0d305.js.map
