109912a8ad05ed568b5ca811dd075679aa8cd296
Dmytro-Hlushchenko <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-04-17 11:37:04 +0000

    Deploying to gh-pages from @ Dmytro-Hlushchenko/goit-js-hw-11@f0a92906ba67c9c1cb51b246ff73d52d8890fea3 🚀

--- index.94d0d305.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.getElementById("search-form");document.querySelector("button");e.addEventListener("submit",(function(e){e.preventDefault(),console.log(e.currentTarget.elements.searchQuery.value)}))}();
-//# sourceMappingURL=index.94d0d305.js.map
--- index.f2fb2888.js
+++
@@ -1,2 +0,0 @@
-const e=document.getElementById("search-form");document.querySelector("button");e.addEventListener("submit",(function(e){e.preventDefault(),console.log(e.currentTarget.elements.searchQuery.value)}));
-//# sourceMappingURL=index.f2fb2888.js.map
