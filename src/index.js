899cc2fde132d80f0c72ee81137a881c98ca2544
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-04-17 16:36:19 +0300

    add-gallery

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Notiflix from 'notiflix';
 
 const KEY_PIXABAY = "32473548-3011831d563d5a9dc36fe58a5";
 const IMG_TYPE = "image_type=photo";
@@ -15,14 +16,20 @@ function onSearchBtn(e) {
     e.preventDefault();
     const query = (e.currentTarget.elements.searchQuery.value).trim();
     if (!query) {
-        alert("no DATA");
+        Notiflix.Notify.failure('Please, enter search words');
         return;
     }
     axios(`https://pixabay.com/api/?key=${KEY_PIXABAY}&q=${query}&${IMG_TYPE}&${ORIENT}&${SAFESEARCH}`)
-        .then(({data}) => createGallery(data.hits))
+        .then(({ data }) => createGallery(data.hits))
         .catch(onErrore);
+       
+        
     
     function createGallery(resultArray) {
+        if (resultArray.length === 0) {
+            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+            return
+            }
         console.log(resultArray);
         const list = resultArray.reduce((markup, item) => markup + createCard(item), "");
         gallery.innerHTML = list;
@@ -30,12 +37,15 @@ function onSearchBtn(e) {
     }
     
     function createCard(item) { 
-         return `<li>
-        <img src = "${item.webformatURL}" alt = "${item.tags}"></img>
-        <p>likes: ${item.likes}</p>
-        <p>views: ${item.views}</p>
-        <p>comments: ${item.comments}</p>
-        <p>downloads: ${item.downloads}</p>
+        return `<li>
+         <a href = "${item.largeImageURL}">
+        <img width = "480" height = "320" src = "${item.webformatURL}" alt = "${item.tags}"></img></a>
+        <div class = "img_comments">
+        <p class = "image_comment">likes <span class = img_comments_vale>${item.likes}</span></p>
+        <p class = "image_comment">views <span class = img_comments_vale>${item.views}</span></p>
+        <p class = "image_comment">comments <span class = img_comments_vale>${item.comments}</span></p>
+        <p class = "image_comment">downloads <span class = img_comments_vale>${item.downloads}</span></p>
+        </div>
         </li>`
     }
         
